930c06487e860def7a1986f8103a5dd102c37697
LPittet1 <pittet82@gmail.com>
2025-02-09 12:39:55 +0100

    uptated structure

---
+++ cpp00/ex00/ex00.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int	main(int ac, char **av)
+{
+	int i = 1;
+	int j;
+
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		while (av[i])
+		{
+			j = 0;
+			while (av[i][j])
+			{
+				std::cout << (char)std::toupper(av[i][j]);
+				j++;
+			}
+			i++;
+		}
+		std::cout << std::endl;
+	}
+}
