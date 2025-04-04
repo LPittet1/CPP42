b8f39280f369ac5df90d4095580a68dc989eec7c
LPittet1 <pittet82@gmail.com>
2025-04-04 19:34:57 +0200

    Account done

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -1,6 +1,7 @@
 #include "Account.hpp"
 #include <iostream>
 #include <ctime>
+#include <iomanip>
 
 int Account::_nbAccounts = 0;
 int Account::_totalAmount = 0;
@@ -10,12 +11,14 @@ int Account::_totalNbWithdrawals = 0;
 Account::Account(int initial_deposit)
 {
 	_amount = initial_deposit;
+	_accountIndex = _nbAccounts;
 	_displayTimestamp();
 	std::cout << " index:" << _accountIndex;
 	std::cout << ";amount:" << _amount;
 	std::cout << ";created" << std::endl;
+	_totalAmount += initial_deposit;
+	_nbAccounts++;
 }
-
 
 Account::~Account()
 {
@@ -48,7 +51,7 @@ int Account::getNbWithdrawals()
 void Account::displayAccountsInfos()
 {
 	_displayTimestamp();
-	std::cout << "accounts:"  << getNbAccounts(); 
+	std::cout << " accounts:"  << getNbAccounts(); 
 	std::cout << ";total:" << getTotalAmount();
 	std::cout << ";deposits:" << getNbDeposits();
 	std::cout << ";withdrawals:" << getNbWithdrawals() << std::endl;
@@ -64,6 +67,8 @@ void Account::makeDeposit(int deposit)
 	_nbDeposits += 1;
 	std::cout << ";amount:" << _amount;
 	std::cout << ";nb_deposits:" << _nbDeposits << std::endl;
+	_totalNbDeposits++;
+	_totalAmount += deposit;
 }
 
 bool Account::makeWithdrawal(int withdrawal)
@@ -82,6 +87,8 @@ bool Account::makeWithdrawal(int withdrawal)
 	_nbWithdrawals += 1;
 	std::cout << ";amount:" << _amount;
 	std::cout << ";nb_withdrawals:" << _nbWithdrawals << std::endl;
+	_totalNbWithdrawals++;
+	_totalAmount -= withdrawal;
 	return true;
 }
 
@@ -96,13 +103,18 @@ void Account::displayStatus() const
 	std::cout << " index:" << _accountIndex;
 	std::cout << ";amount:" << _amount;
 	std::cout << ";deposits:" << _nbDeposits;
-	std::cout << ";withdrawals" << _nbWithdrawals << std::endl;
+	std::cout << ";withdrawals:" << _nbWithdrawals << std::endl;
 }
 
 void Account::_displayTimestamp()
 {
-	std::time_t time = std::time(nullptr);
-	wchar_t str[18];
-	std::wcsftime(str, 18, L"[%Y%m%d_%H%M%S]", std::localtime(&time));
-	std::cout << str;
+	std::time_t time = std::time(&time);
+	std::tm local = *std::localtime(&time);
+	std::cout << "[" << local.tm_year + 1900;
+	std::cout << std::setw(2) << std::setfill('0') << local.tm_mon + 1;
+	std::cout << std::setw(2) << std::setfill('0') << local.tm_mday;
+	std::cout << "_" << std::setw(2) << std::setfill('0') << local.tm_hour;
+	std::cout << std::setw(2) << std::setfill('0') << local.tm_min,
+	std::cout << std::setw(2) << std::setfill('0') << local.tm_sec;
+	std::cout << "]";
 }
