790619cb2d3ceebb16a0993165a3311986d46401
LPittet1 <pittet82@gmail.com>
2025-04-17 11:45:32 +0200

    first struct for ex01

---
+++ cpp03/ex01/ScavTrap.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
+{
+private:
+	/* data */
+public:
+	ScavTrap(/* args */);
+	~ScavTrap();
+};
+
+ScavTrap::ScavTrap(/* args */)
+{
+}
+
+ScavTrap::~ScavTrap()
+{
+}
